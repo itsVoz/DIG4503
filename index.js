f360ac9850202275cd6e85c3787b00b5cf3f167b
itsVoz <33042762+itsVoz@users.noreply.github.com>
2021-02-04 21:49:03 -0500

    Update index.js

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ const customer = {
 
 const output = <div>
   <h1>My name is {customer.first_name}</h1>
-  <h2>My last name is {customer.last_name} </h2>
+  <h2>My last name is {customer.last_name}</h2>
 </div>;
 
 ReactDOM.render(output, document.querySelector("#root"));
