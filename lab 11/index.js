1d46e0151a8db41cb16e70fa30bd1f680d14d2a2
itsVoz <33042762+itsVoz@users.noreply.github.com>
2021-04-27 21:44:50 -0400

    Portfolio 2

--- lab 11/index.js
+++ lab 11/index.js
@@ -1,24 +1,34 @@
 // Importing required packages
 import MongoClient from 'mongodb';
+import chalk from 'chalk';
 
 // Assigning URL to Mongodb server
 const URL = "mongodb+srv://madams:F7Rn240l5cN7siQr@cluster0.oewwc.mongodb.net/test";
 
 // Connecting to db
 // Ensuring that data will be read by all versions
-// Filtering through db with cursor to find all infomation required
-// Logging results and doumenting information found
 MongoClient.connect(URL, {useUnifiedTopology: true})
 .then(connection => {
+
+    // Reading db data
+    // "sample_airbnb" is the target for the connection
     let database = connection.db("sample_airbnb");
 
+    // Compiling the collection 'listingAndReviews'
+    // Assining that collection to 'collection'
     let collection = database.collection("listingsAndReviews");
 
+    // Filtering through db with cursor to find all infomation required
     let cursor = collection.find({price: {$lte: 200}, beds: {$gte: 5}, "review_scores.review_scores_rating": {$gte:99}});
 
+
+    // Logging results and doumenting information found
+    // Added chalk elements for stylistic terminal text
     cursor.forEach(document => {
-        console.log(document.name + ' | beds:' + document.beds + ' | price:' + document.price + ' | review score rating:' + document.review_scores.review_scores_rating);
+        console.log(chalk.bold(document.name) + ' | ' + chalk.bgRed.black('beds:' + document.beds) + ' | price:' + chalk.green(document.price) + ' | review score rating:' + chalk.italic(document.review_scores.review_scores_rating));
     }, ()=> {
+
+        // Closing our connection
         connection.close();
     })
 })
